3ba55b8eb8adaf91809ba7d758122b49c3f7cd88
MC <mahmutcvdr@gmail.com>
2020-01-18 13:08:40 +0300

    inital Turkish class added

---
+++ src/edu/stanford/nlp/international/turkish/TurkishFirst.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package edu.stanford.nlp.international.turkish;
+
+/**
+ * @author Mahmut CAVDAR
+ *
+ */
+public class TurkishFirst {
+
+	/**
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
